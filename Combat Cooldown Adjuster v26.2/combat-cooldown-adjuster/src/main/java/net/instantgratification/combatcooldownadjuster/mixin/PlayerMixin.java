f8adf4081b8a8dc08a981bae52e550dd7387076d
Rifa <fm.rifaditya@gmail.com>
2026-09-14 10:54:58 +0700

    fix(26.2): remove invalid @Shadow getMainHandItem in PlayerMixin

--- Combat Cooldown Adjuster v26.2/combat-cooldown-adjuster/src/main/java/net/instantgratification/combatcooldownadjuster/mixin/PlayerMixin.java
+++ Combat Cooldown Adjuster v26.2/combat-cooldown-adjuster/src/main/java/net/instantgratification/combatcooldownadjuster/mixin/PlayerMixin.java
@@ -1,6 +1,6 @@
 package net.instantgratification.combatcooldownadjuster.mixin;
 
-// Verified against: Player.java (Minecraft 26.1.2)
+// Verified against: Player.java (Minecraft 26.2)
 
 import net.instantgratification.combatcooldownadjuster.registry.CombatRules;
 import net.instantgratification.combatcooldownadjuster.util.CCAHooks;
@@ -8,7 +8,6 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,12 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Player.class)
 public abstract class PlayerMixin {
 
-    @Shadow public abstract ItemStack getMainHandItem();
-
     @Inject(method = "getCurrentItemAttackStrengthDelay", at = @At("HEAD"), cancellable = true)
     private void cca$overrideAttackDelay(CallbackInfoReturnable<Float> cir) {
         Player player = (Player) (Object) this;
-        ItemStack stack = this.getMainHandItem();
+        ItemStack stack = player.getMainHandItem();
         int ticks = CCAHooks.getCooldownTicks(player, stack);
         if (ticks >= 0) {
             cir.setReturnValue((float) ticks);
